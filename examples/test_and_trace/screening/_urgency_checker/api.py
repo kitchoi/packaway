b5d5398677d9087fc7f740e8706395e50867a8ca
Kit Choi <kit@kychoi.org>
2020-09-26 17:59:01 +0100

    More fake structure

---
+++ examples/test_and_trace/screening/_urgency_checker/api.py
@@ -0,0 +1,6 @@
+def get_form():
+    return {}
+
+
+def is_eligible(data):
+    return False
--- examples/test_and_trace/screening/api.py
+++ examples/test_and_trace/screening/api.py
@@ -1,16 +1,16 @@
-import test_and_trace.screening._symptom_checker.api as symptom_checker
-import test_and_trace.screening._urgency_checker.api as urgency_checker
+from ._symptom_checker import api as symptom_checker_api
+from ._urgency_checker import api as urgency_checker_api
 
 
 def get_form():
     return {
-        "symptom": symptom_checker.get_form(),
-        "urgency": urgency_checker.get_form(),
+        "symptom": symptom_checker_api.get_form(),
+        "urgency": urgency_checker_api.get_form(),
     }
 
 
 def is_eligible(data):
     return (
-        symptom_checker.is_eligible(data["symptom"]) and
-        urgency_checker.is_eligible(data["urgency"])
+        symptom_checker_api.is_eligible(data["symptom"]) and
+        urgency_checker_api.is_eligible(data["urgency"])
     )
