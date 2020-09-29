4da85a34b60cc0cd2e8c6cd729df4cfc9ed95fec
Kit Choi <kit@kychoi.org>
2020-09-29 19:55:30 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ setuptools.setup(
     license="MIT",
     version="0.1.0",
     description="Check imports for enforcing encapsulation.",
-    packages=setuptools.find_packages(
-        where='packaway',
-    ),
+    packages=setuptools.find_packages(),
     entry_points={
         "flake8.extension": [
             'ARC = packaway.plugins.flake8.import_checker:ImportChecker',
