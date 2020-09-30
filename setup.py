a3e73328a1250f70c636fd325b00d6e22d43d4fa
Kit Choi <kit@kychoi.org>
2020-09-30 16:34:40 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,27 @@ HERE = os.path.dirname(__file__)
 VERSION_FILE = os.path.join(HERE, "packaway", "version.py")
 VERSION = runpy.run_path(VERSION_FILE)["__version__"]
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="packaway",
     license="MIT",
     version=VERSION,
-    description="Check imports for enforcing encapsulation.",
+    description="Static Checker to Enforce (Some) Encapsultion in Python.",
     packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/kitchoi/packaway",
     entry_points={
         "flake8.extension": [
             'DEP = packaway.plugins.flake8.import_checker:ImportChecker',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
