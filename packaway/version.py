cb9186519ae259cd382895a56b200403c7b8cbb0
Kit Choi <kit@kychoi.org>
2020-09-30 16:43:21 +0100

    Bump version

--- packaway/version.py
+++ packaway/version.py
@@ -1,10 +1,10 @@
 
 _MAJOR = 0
-_MINOR = 1
-_MICRO = 2
+_MINOR = 2
+_MICRO = 0
 
 #: One of 'alpha', 'beta', 'candidate', 'final'
-_RELEASE_LEVEL = "final"
+_RELEASE_LEVEL = "alpha"
 
 _SERIAL = 0
 
