e6e2dd435f3c856edd2a94d651b2990da32b8ac6
Kit Choi <kit@kychoi.org>
2020-10-03 17:49:12 +0100

    Update example output

--- examples/package/__init__.py
+++ examples/package/__init__.py
@@ -5,7 +5,7 @@ imports.
 Running the flake8 plugin 'packaway-import' should result in a number of
 errors::
 
-    $ flake8 --ignore F401 .
+    $ flake8 --ignore F401 --top-level-dir ../ .
     ./office/api.py:7:1: DEP401 Importing private name 'person._reading'.
     ./office/api.py:13:1: DEP401 Importing private name '_legal._compliance'.
     ./office/_hours.py:2:1: DEP401 Importing private name '_accounting._booking'.
@@ -15,4 +15,4 @@ errors::
 
 For demo purposes, imports are unused and therefore the F401 error code is
 ignored.
-"""
+"""  # noqa: E501
