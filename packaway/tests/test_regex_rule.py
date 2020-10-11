7727b34f97b3cdd7b7a81fa38110a94d9e699230
Kit Choi <kit@kychoi.org>
2020-10-11 13:38:35 +0100

    Add more tests for the regex rule

--- packaway/tests/test_regex_rule.py
+++ packaway/tests/test_regex_rule.py
@@ -23,3 +23,62 @@ class TestRegexRule(unittest.TestCase):
             "Import 'gui_package.api.x' violates pattern: '^gui_package'",
             error.message,
         )
+
+    def test_disallowed_many_regex(self):
+        disallowed_patterns = [
+            r".*\.gui\..*",
+            r".*\.web\..*",
+        ]
+        bad_sources = [
+            "from package.gui.api import x",
+            "from .subpackage.web.api import x",
+        ]
+        for source in bad_sources:
+            with self.subTest(source=source):
+                tree = ast.parse(source)
+                errors = collect_errors(
+                    tree=tree,
+                    module_name=None,
+                    disallowed_patterns=disallowed_patterns,
+                )
+                self.assertEqual(len(errors), 1)
+
+    def test_disallowed_many_regex_with_module_name(self):
+        # Test when the source module name is known.
+        source_module = "package.module2"
+        disallowed_patterns = [
+            r".*\.gui\..*",
+            r".*\.web\..*",
+        ]
+        bad_sources = [
+            "from package.gui.api import x",
+            "from .web.api import x",  # resolve to 'package.web.api'
+        ]
+        for source in bad_sources:
+            with self.subTest(source=source):
+                tree = ast.parse(source)
+                errors = collect_errors(
+                    tree=tree,
+                    module_name=source_module,
+                    disallowed_patterns=disallowed_patterns,
+                )
+                self.assertEqual(len(errors), 1)
+
+    def test_disallowed_none_match(self):
+        disallowed_patterns = [
+            r".*\.gui\..*",
+            r".*\.web\..*",
+        ]
+        good_sources = [
+            "from package.api import x",
+            "from .web.api import x",   # resolve to just 'web.api.x'
+        ]
+        for source in good_sources:
+            with self.subTest(source=source):
+                tree = ast.parse(source)
+                errors = collect_errors(
+                    tree=tree,
+                    module_name=None,
+                    disallowed_patterns=disallowed_patterns,
+                )
+                self.assertEqual(len(errors), 0)
