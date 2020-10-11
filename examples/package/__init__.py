c37972ce563f2b0ace6743cfc7c2c8db63b2fe84
Kit Choi <kit@kychoi.org>
2020-10-11 16:02:40 +0100

    Add a local config file in example

--- examples/package/__init__.py
+++ examples/package/__init__.py
@@ -5,7 +5,7 @@ imports.
 Running the flake8 plugin 'packaway-import' should result in a number of
 errors::
 
-    $ flake8 --ignore F401 --top-level-dir ../ .
+    $ flake8 .
     ./office/api.py:7:1: DEP401 Importing private name 'person._reading'.
     ./office/api.py:13:1: DEP401 Importing private name '_legal._compliance'.
     ./office/_hours.py:2:1: DEP401 Importing private name '_accounting._booking'.
@@ -14,5 +14,5 @@ errors::
     ./person/api.py:5:1: DEP401 Importing private name '_reading._private_name'.
 
 For demo purposes, imports are unused and therefore the F401 error code is
-ignored.
+ignored, see setup.cfg for the local configuration.
 """  # noqa: E501
