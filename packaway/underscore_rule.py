58c3ce82ad98e7bd67b52338505ae83b5ac48236
Kit Choi <kit@kychoi.org>
2020-10-11 12:15:04 +0100

    Extract module name normalization logic.

--- packaway/underscore_rule.py
+++ packaway/underscore_rule.py
@@ -23,17 +23,20 @@ def _is_valid_import(source_module, target_module, level=0):
         of parent directories to search relative to the directory
         of the module calling import.
     """
-    target_parts = target_module.split(".")
 
     if source_module is None:
         source_parts = []
     else:
         source_parts = source_module.split(".")
 
-        if level > len(source_parts):
-            raise ValueError("Level is too deep.")
+    target_module = _normalize_target_module(
+        source_module=source_module,
+        target_module=target_module,
+        level=level,
+    )
 
-        target_parts = source_parts[:-level] + target_parts
+    level = 0
+    target_parts = target_module.split(".")
 
     pairs = zip(
         source_parts,
@@ -50,6 +53,35 @@ def _is_valid_import(source_module, target_module, level=0):
             return False
     else:
         return True
+
+
+def _normalize_target_module(source_module, target_module, level):
+    """ Normalize relative import to absolute import.
+
+    Parameters
+    ----------
+    source_module : str or None
+        Name of the module where the import is written.
+        If given, this name should be absolute.
+    target_module : str
+        Name of the module being imported.
+        This name can be absolute or relative depending on the value
+        of ``level``.
+    level : int, optional
+        level specifies whether to use absolute or relative
+        imports. 0 (the default) means only perform absolute
+        imports. Positive values for level indicate the number
+        of parent directories to search relative to the directory
+        of the module calling import.
+    """
+    if source_module is None or level == 0:
+        return target_module
+
+    source_parts = source_module.split(".")
+    if level > len(source_parts):
+        raise ValueError("Level is too deep.")
+    target_parts = source_parts[:-level] + target_module.split(".")
+    return ".".join(target_parts)
 
 
 def collect_errors(tree, module_name=None):
